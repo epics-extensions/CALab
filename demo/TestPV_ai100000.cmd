ed07b77310e3f25dd6a456eb1e1c8b10fbd05c1f
Carsten Winkler <carsten.winkler@helmholtz-berlin.de>
2022-08-03 16:19:30 +0200

    additional example for the configuration of the SoftIOC this is used by the example "LabVIEW\user.lib\caLab\Examples\LV2019\Event Demo.vi"

---
+++ demo/TestPV_ai100000.cmd
@@ -0,0 +1,10 @@
+@echo off
+
+echo start demo IOC now
+echo Type 'dbl' to get a list of all available records.
+echo Type 'exit' to terminate this session.
+echo ---
+
+..\lib\softIoc.exe -D ..\lib\softIoc.dbd -d db\TestPV_ai100000.db
+
+:end
